822e79839bdffa8e563e6432406c3b17be8018ce
Claudio Jolowicz <cj@dealloc.org>
2011-04-03 18:10:03 +0200

    Create Rails default directory structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hadiya::Application.config.secret_token = '471192b0ddbe4525f1453b2c75bc71b659000ab440e777fc553506616c8f3c1fa0fc768b1c31e6658a42a2521c2791552ad703f63255903f0867c07fe7fa78b0'
